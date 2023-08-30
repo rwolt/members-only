996fcf0b2343352409cb1d2475690f14af823221
Ray Wolt <rjwolt@gmail.com>
2023-08-30 11:57:59 -0500

    Add route for posting a new message

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const passport = require("passport");
 const User = require("../models/user");
+const Message = require("../models/message");
 
 /* GET home page. */
 router.get("/", (req, res, next) => {
@@ -115,5 +116,42 @@ router.get("/join", (req, res, next) => {
 router.get("/message", (req, res, next) => {
   res.render("message", { title: "Members Only New Message" });
 });
+
+router.post(
+  "/message",
+  body("title", "Title is required").trim().isLength({ min: 1 }).escape(),
+  body("title", "Message text is required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const message = {
+        title: req.body.title,
+        text: req.body.text,
+      };
+      return res.render("message", {
+        title: "Members Only New Message",
+        errors: errors.array(),
+        message: message,
+      });
+    } else {
+      const timestamp = new Date();
+      const newMessage = new Message({
+        title: req.body.title,
+        text: req.body.text,
+        timestamp: timestamp,
+      });
+      try {
+        const result = await newMessage.save();
+        console.log(result);
+        res.redirect("/posts");
+      } catch (err) {
+        return next(err);
+      }
+    }
+  }
+);
 
 module.exports = router;
